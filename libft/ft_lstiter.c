9f7db765416c971e6866cb60be1177b620c53fc8
RnSiilva <renan.carlossilva@hotmail.com>
2023-10-16 02:48:20 +0100

    att_ltsiter

--- libft/ft_lstiter.c
+++ libft/ft_lstiter.c
@@ -6,13 +6,29 @@
 /*   By: resilva <resilva@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/16 02:21:27 by resilva           #+#    #+#             */
-/*   Updated: 2023/10/16 02:22:36 by resilva          ###   ########.fr       */
+/*   Updated: 2023/10/16 02:47:56 by resilva          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 void ft_lstiter(t_list *lst, void (*f)(void *))
+{
+	if (!lst || !f)
+		return ;
+	while (lst)
+	{
+		f(lst->content);
+		lst = lst->next;
+	}
+}
+
+void	ft_iter(void *content)
+{
+
+}
+
+int	main(void)
 {
 	
 }
